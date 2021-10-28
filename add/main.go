ecefb6b5ae24f08d7e5777d2220c1666efa5bffb
“Kaustubh <kaustubh@codedsa.com>
2021-10-28 23:15:29 +0000

    server client

---
+++ add/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net"
+	"fmt"
+	"github.com/CodeDSA/hello/pb"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func (s *server) Compute(cxt context.Context, r *pb.GCDRequest) (*pb.GCDResponse , error){
+	result := &pb.GCDResponse{}
+	result.Result = r.A + r.B
+	logMessage := fmt.Sprintf("A: %d    B: %d    sum: %d", r.A, r.B, result.Result)
+	log.Println(logMessage)
+
+	return result, nil
+}
+
+type server struct{}
+
+func main(){
+	lis, err := net.Listen("tcp", ":3000")
+
+	if err != ni{
+	log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGCDServiceServer(s , &server{})
+	reflection.Register(s)
+
+	if err := s.Serve(lis); err != nil {
+	log.Fatlf("Failed to serve: %v", err)
+	}
+}
+
